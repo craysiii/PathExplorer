13fdae9d4fcf31ec14e74fba394aa79be2c839a9
stanislavromanov <stanislav.romanov@outlook.com>
2014-09-10 14:56:58 +0300

    extract classes as files

---
+++ PathExplorer/Path.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel;
+using System.IO;
+
+namespace PathExplorer
+{
+    public class Path
+    {
+        private string value;
+
+        [DisplayName("Path Variable")]
+        public string Value
+        {
+            get
+            {
+                return this.value;
+            }
+            set
+            {
+                if (File.Exists(value) || Directory.Exists(value))
+                {
+                    this.value = value;
+                }
+
+                else
+                {
+                    throw new FileNotFoundException(@value + "does not exist.");
+                }
+            }
+        }
+
+        public Path(string value)
+        {
+            this.Value = value;
+        }
+
+    }
+}
---
+++ PathExplorer/PathExplorer.cs
@@ -0,0 +1,53 @@
+﻿using System.ComponentModel;
+using Microsoft.Win32;
+
+namespace PathExplorer
+{
+    public class PathExplorer
+    {
+        private readonly string PathKey = @"HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment";
+        BindingList<Path> paths = new BindingList<Path>();
+
+        public PathExplorer()
+        {
+            string temp = (string)Registry.GetValue(PathKey, "Path", null);
+            var list = temp.Split(';');
+            foreach (string item in list)
+            {
+                this.addPath(item);
+            }
+
+        }
+
+        public BindingList<Path> Paths
+        {
+            get
+            {
+                return this.paths;
+            }
+        }
+
+        public void addPath(string value)
+        {
+            var path = new Path(value);
+            this.paths.Add(path);
+
+        }
+
+        public void deletePath(int index)
+        {
+            this.paths.RemoveAt(index);
+        }
+
+        public void commitPath()
+        {
+            string absolutePath = "";
+            foreach (Path path in this.paths)
+            {
+                absolutePath += (path.Value + ';');
+            }
+
+            Registry.SetValue(PathKey, "Path", absolutePath.Substring(0, absolutePath.Length - 1));
+        }
+    }
+}
